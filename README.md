4537e7adfe113cc8352f28478fbe9300d990d5b7
Quincy Michael <michaelkirima865@gmail.com>
2024-09-26 15:08:58 +0300

    New Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Code-Challenge-Phase-1-Week-1
+# (Quincy's Code Challenge)
+
+#### Date, 28/09/2024
+
+#### By *Quincy Michael*
+
+## Description
+It's a webpage that does the following:
+1.Students Grade
+2.Detects Speed Limit
+3.Calculates Net Salary
+
+## Installation
+You use git clone to be able to download the documents in the GitHub
+
+## Installation Requirements
+Git
+
+### Installation instruction
+```
+Git clone https://quincy865.github.io/Code-Challenge-Phase-1-Week-1/
+
+```
+
+## Technologies used
+Github
+Javascript
+
---
+++ Student/index.js
@@ -0,0 +1,20 @@
+function studentGrade(marks) {
+    if (marks > 100 || marks < 0) {
+        return "Inavlid kindly input marks ranging from 0 - 100"
+    } else if (marks > 79) {
+        return "Grade: A";
+    } else if (marks >= 60 && marks <= 79) {
+        return "Grade: B";
+    } else if (marks >= 49 && marks <= 59) {
+        return "Grade: C";
+    } else if (marks >= 40 && marks <= 49) {
+        return "Grade: D";
+    } else if (marks < 40) {
+        return "Grade: E";
+    }
+}
+
+
+let marks = parseInt(prompt("Enter the students marks (between 0 - 100)"));
+
+console.log(studentGrade(marks));
